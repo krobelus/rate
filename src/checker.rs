3dbbdd083a848c19fdc0b04596b03b1097d76914
Johannes Altmanninger <aclopte@gmail.com>
2019-02-10 02:46:51 +0100

    don't write deletion of missing clauses

--- src/checker.rs
+++ src/checker.rs
@@ -1247,12 +1247,14 @@ fn write_lrat_certificate(checker: &mut Checker) -> io::Result<()> {
                     }
                 }
                 ProofStep::Deletion(clause) => {
-                    invariant!(
-                        (lrat_id(checker, clause) == Clause::UNINITIALIZED)
-                            == (clause >= checker.lemma && !checker.clause_scheduled[clause])
-                    );
-                    if lrat_id(checker, clause) != Clause::UNINITIALIZED {
-                        write_deletion(checker, &mut file, &mut clause_deleted, clause)?;
+                    if clause != Clause::DOES_NOT_EXIST {
+                        invariant!(
+                            (lrat_id(checker, clause) == Clause::UNINITIALIZED)
+                                == (clause >= checker.lemma && !checker.clause_scheduled[clause])
+                        );
+                        if lrat_id(checker, clause) != Clause::UNINITIALIZED {
+                            write_deletion(checker, &mut file, &mut clause_deleted, clause)?;
+                        }
                     }
                     i += 1;
                 }
