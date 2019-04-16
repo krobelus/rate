3923ba006a3a3ca5d1bb83158491a71287017e95
Johannes Altmanninger <aclopte@gmail.com>
2019-04-17 00:00:55 +0200

    split & check in parallel

--- scripts/split-and-check.sh
+++ scripts/split-and-check.sh
@@ -2,8 +2,6 @@
 
 set -eu
 
-# TODO this assumes the proof is textual
-
 formula="$1"
 proof="$2"
 ext="${proof##*.}"
@@ -11,8 +9,6 @@ ext="${proof##*.}"
 N="${3:-2}"
 verbose="${4:-}"
 logN="$(python3 -c "from math import log, ceil; print(ceil(log($N, 10)))")"
-lines="$(wc -l < "$proof")"
-lines_per_chunk="$(python3 -c "from math import floor; print(floor($lines / $N))")"
 
 checker() {
 	drat-trim "$@" -f -I
@@ -26,15 +22,6 @@ log() {
 
 log "Splitting proof in $N chunks"
 
-# produces "$proof".XXX.drat
-#TODO
-# split "$proof" "$proof". \
-# 	--additional-suffix=".$ext" \
-# 	--suffix-length=$logN --numeric-suffixes \
-# 	--lines="$lines_per_chunk"
-	# --number=l/$N 
-
-# produces "$proof".XXX.sed
 split-proof "$formula" "$proof" "$N"
 
 check_part() {
@@ -52,19 +39,17 @@ check_part() {
 	log "patched formula: $patched_formula"
 	log "stored in: $proof.$id.xcnf"
 	log "proof chunk: $proof_chunk"
-        # TODO remove this
-        sh -c "$patched_formula" > "$proof".$id.xcnf
 	sh -c "$patched_formula" | checker /dev/stdin "$proof_chunk"
 }
 
-for part in $(seq 0 $((N - 2)))
-do
-        # out="$(check_part $part ||:)"
-        # echo "$out"
-	# echo "$out" | grep 'early EOF' && exit 123 ||:
-	check_part $part |
-	grep -q -E '(c ERROR: all lemmas verified|s VERIFIED)'
-done
-check_part $((N - 1)) |
-	tee /dev/stderr |
-grep -q 's VERIFIED'
+# TODO use env_parallel.sh
+. "$(which env_parallel.bash)"
+
+(
+    for part in $(seq 0 $((N - 2)))
+    do
+	printf %s\\n "check_part $part | \
+            	grep -q -E '(c ERROR: all lemmas verified|s VERIFIED)'"
+    done
+    printf %s\\n "check_part $((N - 1)) | grep -q 's VERIFIED'"
+) | env_parallel
