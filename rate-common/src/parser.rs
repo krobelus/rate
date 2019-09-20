ea5923473c1f0de3bebc6e8c0309394725ed2c77
Adrian Rebola-Pardo <arpj.rebola@gmail.com>
2019-09-20 20:44:26 +0200

    Added ParsedClause struct

--- rate-common/src/parser.rs
+++ rate-common/src/parser.rs
@@ -830,6 +830,12 @@ fn parse_clause(
     }
 }
 
+enum ParsedClause {
+    Tautology,
+    Clause,
+    Repetition(Literal),
+}
+
 fn parse_any_clause(
     parser: &mut Parser,
     clause_ids: &mut impl HashTable,
