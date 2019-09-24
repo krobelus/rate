cab431ed4bcfdc0306e973d1621a6dacd656d804
Johannes Altmanninger <aclopte@gmail.com>
2019-09-24 17:30:33 +0200

    fix sick-check call

--- test.py
+++ test.py
@@ -236,12 +236,13 @@ def double_check(drat_checker,
         name = cnf[:-len('.cnf')] if cnf.endswith('.cnf') else cnf
         pr = os.path.exists(f'{name}.dpr')
         args = [cnf]
-        # NOTE file extension .pr is disallowed, use dpr TODO adjust docs
-        args += [f'--proof-format={os.path.splitext(proof)[1][1:]}']
         if pr:
             args += [f'{name}.dpr']
         else:
             args += [proof]
+        # NOTE file extension .pr is disallowed, use dpr TODO adjust docs
+        args += [f'--proof-format={os.path.splitext(proof)[1][1:]}']
+        if not pr:
             if lrat:
                 args += ['-L', f'{name}.lrat']
             if grat:
