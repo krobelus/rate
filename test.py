48f16174e838462d21d16450f8811dca953d905c
Johannes Altmanninger <aclopte@gmail.com>
2019-12-28 17:12:02 +0100

    simplify tests

--- test.py
+++ test.py
@@ -2,40 +2,26 @@
 
 import time
 import os
+import distutils.spawn
 from subprocess import Popen, PIPE
-from functools import lru_cache
-
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
+import sys
 
 benchmark_location = 'benchmarks'
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-INITIAL_COMMIT = '39d6db9faa1b1c3c252fcd1a41b5156ffb0a97b2'
+cnfs = [f'{dirpath}/{file}'
+            for dirpath, _, files in os.walk(benchmark_location)
+            for file in files
+            if file.endswith('.cnf') and '/excluded' not in dirpath]
 
-
-def run(command):
-    assert Popen(command).wait() == 0
-
-
-def build_release(commit_sha=None):
+def build_release():
     cargo_release = ['cargo', 'build', '--release']
-    if commit_sha:
-        return run(['./scripts/exec-in-version.sh', commit_sha] +
-                   cargo_release)
     run(cargo_release)
 
 
-def rate(commit_sha=None, *, flags=[]):
+def rate(flags=[]):
     executable = './target/release/rate'
-    if commit_sha:
-        executable = f'./checkouts/{commit_sha}/{executable}'
     return [executable] + flags
-
-
-def benchmark_cnfs():
-    return (f'{dirpath}/{file}'
-            for dirpath, _, files in os.walk(benchmark_location)
-            for file in files
-            if file.endswith('.cnf') and '/excluded' not in dirpath)
 
 
 def size(cnf):
@@ -45,7 +31,7 @@ def size(cnf):
 def drat_inputs():
     return [
         (cnf, (cnf[:-len('cnf')] + 'drat'))
-        for cnf in sorted(benchmark_cnfs(), key=size)
+        for cnf in sorted(cnfs, key=size)
         if os.path.exists(cnf[:-len('cnf')] + 'drat')
     ]
 
@@ -60,69 +46,61 @@ def small_drat_inputs():
 
 def pr_inputs():
     return [
-        (cnf, cnf[:-len('cnf')] + 'dpr') for cnf in sorted(benchmark_cnfs(), key=size)
+        (cnf, cnf[:-len('cnf')] + 'dpr') for cnf in sorted(cnfs, key=size)
         if os.path.exists(cnf[:-len('cnf')] + 'dpr')
     ]
 
 
 def executable(name):
-    return Popen(('which', name)).wait() == 0
+    return distutils.spawn.find_executable(name)
 
 
 def ensure_executable(command):
     assert executable(command[0]), f'{command[0]} not found in PATH'
 
 
+def popen(command, **kwargs):
+    print(' '.join(command))
+    return Popen(command, **kwargs)
+
+
+def run(command):
+    assert popen(command).wait() == 0
+
+
 def process_expansion(command, input=None):
     # we need to redirect stderr for gratgen
-    p = Popen(command, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    p = popen(command, stdin=PIPE, stdout=PIPE, stderr=PIPE)
     if input is not None:
         return p.communicate(input=input)
     else:
         return p.communicate()
 
 
-@lru_cache(maxsize=None)
+process_expansion_cache = {}
 def process_expansion_cached(command):
     assert isinstance(command, tuple)
+    if command in process_expansion_cache:
+        return process_expansion_cache[command]
     return process_expansion(command)
 
 
-def timed(f):
-    def wrapper(*args, **kwargs):
-        start = time.time()
-        call = f.__name__ + '(' + ', '.join(str(a) for a in args)
-        if kwargs:
-            call += ',' ', '.join(
-                f'{key}={str(kwargs[key])},' for key in kwargs)
-        call += ')'
-        result = f(*args, **kwargs)
-        duration = time.time() - start
-        if duration > 0.5:
-            print(f'spent %3.2f in {call}' % duration)
-        return result
-
-    return wrapper
-
-
-@timed
 def accepts(checker, name):
     'we take name here to see the benchmark name as a test fails'
     # n.b. this assumes that the DIMACS and DRAT files do not change!
     stdout, stderr = process_expansion_cached(tuple(checker))
     accepted = b's VERIFIED\n' in stdout or (
-        ('rupee' in checker[0] or INITIAL_COMMIT in checker[0])
+        ('rupee' in checker[0])
         and b's ACCEPTED' in stdout) or ('gratgen' in checker[0]
                                          and b's VERIFIED' in stderr)
     rejected = b's NOT VERIFIED\n' in stdout or (
-        ('rupee' in checker[0] or INITIAL_COMMIT in checker[0])
+        ('rupee' in checker[0])
         and b's REJECTED' in stdout) or ('gratgen' in checker[0]
                                          and b's VERIFIED' not in stderr)
     assert accepted != rejected, str(stdout, 'utf8') + str(stderr, 'utf8')
     return accepted
 
 
-@timed
 def lrat_checker_accepts(checker, name):
     stdout, _ = process_expansion(checker)
     ok = (('lratcheck' in checker[0] and b's ACCEPTED\n' in stdout)
@@ -132,7 +110,6 @@ def lrat_checker_accepts(checker, name):
     return ok
 
 
-@timed
 def gratchk_accepts(grat_checker, name):
     stdout, _ = process_expansion(grat_checker)
     ok = b's VERIFIED UNSAT' in stdout
@@ -141,7 +118,6 @@ def gratchk_accepts(grat_checker, name):
     return ok
 
 
-@timed
 def sick_checker_accepts(checker, name):
     stdout, _ = process_expansion(checker)
     ok = b's VERIFIED\n' in stdout
@@ -179,23 +155,6 @@ def compare_acceptance(a, b, *, instances):
                 'benchmarks/crafted/sick-example',
                     'benchmarks/crafted/crlf'):
                 continue  # drat-trim uses binary mode here
-        if INITIAL_COMMIT in b[0]:
-            if name in (
-                'benchmarks/crafted/falsified',
-                    'benchmarks/crafted/crlf',
-                    'benchmarks/crafted/crash',
-                    'benchmarks/crafted/unpropagate',
-            ):
-                continue  # does not skip unused lemmas
-        if (INITIAL_COMMIT in b[0]
-                and name in ('benchmarks/crafted/marked-environment',
-                             'benchmarks/crafted/missing-last-zero',
-                             'benchmarks/crafted/missing-last-zero2',
-                             'benchmarks/crafted/comments')):
-            print(
-                f'skipping {name} as {b[0]} checks all RAT candidates (not just core)'
-            )
-            continue
         if 'gratgen' in b[0]:
             if name in (
                     'benchmarks/crafted/bottom',
@@ -210,6 +169,7 @@ def compare_acceptance(a, b, *, instances):
                 continue
 
         assert accepts(a + args, name) == accepts(b + args, name)
+        print()
 
 
 def double_check(drat_checker,
@@ -268,6 +228,7 @@ def double_check(drat_checker,
         elif sick:
             assert sick_checker_accepts(
                 ['target/release/sick-check'] + args[:2] + [args[-1]], name)
+        print()
 
 
 def test_pr():
@@ -314,14 +275,6 @@ def test_forward():
         instances=set(
             drat_inputs()) | set(
                 pr_inputs()))
-
-# def test_acceptance_initial_commit():
-#     build_release(INITIAL_COMMIT)
-#     compare_acceptance(
-#         rate(flags=['--noncore-rat-candidates']),
-#         rate(INITIAL_COMMIT),
-#         instances=small_drat_inputs())
-
 
 def test_acceptance_drat_trim():
     if executable('drat-trim'):
